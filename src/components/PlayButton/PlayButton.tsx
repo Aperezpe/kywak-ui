f608e7ae55502e0f9b6b30069a3271d9de1bb6e3
Abraham Perez <aperezpe01@gmail.com>
2023-03-28 18:57:37 -0500

    makes play button

--- src/components/PlayButton/PlayButton.tsx
+++ src/components/PlayButton/PlayButton.tsx
@@ -5,7 +5,7 @@ type Props = {}
 
 const PlayButton = (props: Props) => {
   return (
-    <div className='play-button rounded-full bg-white'>
+    <div className='play-button rounded-full'>
       <div className='play-button__middle-fill'></div>
     </div>
   )
